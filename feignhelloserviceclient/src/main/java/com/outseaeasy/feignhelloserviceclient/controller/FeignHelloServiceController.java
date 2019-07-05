07ed7b9590ff7a6b31782c4553fe80602cdc0f11
unrealinux <13653418149@163.com>
2019-07-05 22:19:08 +0800

    提交配置中心模块

--- feignhelloserviceclient/src/main/java/com/outseaeasy/feignhelloserviceclient/controller/FeignHelloServiceController.java
+++ feignhelloserviceclient/src/main/java/com/outseaeasy/feignhelloserviceclient/controller/FeignHelloServiceController.java
@@ -2,7 +2,6 @@ package com.outseaeasy.feignhelloserviceclient.controller;
 
 import com.outseaeasy.feignhelloserviceclient.service.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
