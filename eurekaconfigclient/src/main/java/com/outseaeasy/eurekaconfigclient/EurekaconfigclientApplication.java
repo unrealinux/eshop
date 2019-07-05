b5f3367b71f5b64636ae41405d9ccd8a1a64dabd
unrealinux <13653418149@163.com>
2019-07-05 23:39:42 +0800

    提交配置中心模块

---
+++ eurekaconfigclient/src/main/java/com/outseaeasy/eurekaconfigclient/EurekaconfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.outseaeasy.eurekaconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaconfigclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaconfigclientApplication.class, args);
+    }
+
+}
